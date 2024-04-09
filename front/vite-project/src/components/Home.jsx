0d86aa6d4af85e8d12ed3e9a43c1b7ee5442b9fa
cfcamilafabre <camilafabre98@gmail.com>
2024-04-09 19:41:05 -0300

    instalaciones

---
+++ front/vite-project/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+    <div>
+    <h1> Home </h1>
+    <Navbar/>
+    </div>
+)
+}
+export default Home; 
---
+++ front/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Sobre nosotros</a></li>
+                <li><a href="">Contacto</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </div>
+    )
+}
+export default Navbar; 
