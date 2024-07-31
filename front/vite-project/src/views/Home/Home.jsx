0da18b9910922e109b44e7fcdf34ec14e8caa3da
Camila Fabre <camilafabre98@gmail.com>
2024-07-31 15:50:54 -0300

    footer parte 1

--- front/vite-project/src/views/Home/Home.jsx
+++ front/vite-project/src/views/Home/Home.jsx
@@ -8,6 +8,7 @@ import Services from '../../components/Services/Services';
 import Method from '../../components/Method/Method';
 import Reviews from '../../components/Reviews/Reviews';
 import MapComponent from '../../components/Maps/MapComponent';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
 
@@ -33,20 +34,7 @@ const Home = () => {
         <Method></Method>
         <Reviews></Reviews>
         <MapComponent></MapComponent>
-            <div className={styles.containerHome}>
-                <h1 className={styles.h1Home}> ¡Bienvenido a tuTurno! </h1>
-                <h4 className={styles.pHome}>Ahora reservar tu turno es mucho más sencillo. Podes agendar día y horario haciendo click en el botón. <br /> Recordá que nos podes encontrar de lunes a viernes, de 9:00 a 18:00hs.</h4>
-                {!isLogged &&
-                <>
-                <div className={styles.buttonContainer}>
-                    <Link to='/login'><span className='btn btn-primary'>Inicia sesión para continuar</span></Link>
-                </div>
-                <div className={styles.crearCuentaDiv}>
-                    <p style={{ marginRight: "8px" }}>Todavia no tenes una cuenta?</p>
-                    <Link to='/register'>Registrate ahora!</Link>
-                </div>
-                </>}
-            </div>
+        <Footer></Footer>
         </>
     )
 }
